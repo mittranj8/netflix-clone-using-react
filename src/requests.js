d0f39990e0812a1ccbdeb3d3623490e352b9e550
Mittranj <mpansuriya@unomaha.edu>
2020-11-17 15:24:48 -0600

    small fix

--- src/requests.js
+++ src/requests.js
@@ -1,6 +1,6 @@
 const API_KEY = "aa0d81950b319f2847c77bb523302e62";
 
-const requests = {
+export default {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
   fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
@@ -9,7 +9,6 @@ const requests = {
   fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
   fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
   fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
-
   fetchMysteryMovies: `/discover/movie?api_key=${API_KEY}&with_genres=9648`,
   fetchSciFiMovies: `/discover/movie?api_key=${API_KEY}&with_genres=878`,
   fetchWesternMovies: `/discover/movie?api_key=${API_KEY}&with_genres=37`,
