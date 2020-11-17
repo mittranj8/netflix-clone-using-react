7c32fd9744bbfef15035331fb1f0297bd8000a8e
Mittranj <mpansuriya@unomaha.edu>
2020-11-17 15:24:18 -0600

    added various components ro app component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import "./App.css";
+import Row from "./Row";
+import requests from "./requests";
+import Navmenu from "./Navmenu";
+import Banner from "./Banner";
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello</h1>
+      <Navmenu />
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Mystery Movies" fetchUrl={requests.fetchMysteryMovies} />
+      <Row title="Sci Fi Movies" fetchUrl={requests.fetchSciFiMovies} />
+      <Row title="Animation Movies" fetchUrl={requests.fetchAnimationMovies} />
+      <Row title="Western Movies" fetchUrl={requests.fetchWesternMovies} />
+      <Row title="TV Series and Movies" fetchUrl={requests.fetchTV} />
+      {/* <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} /> */}
     </div>
   );
 }
